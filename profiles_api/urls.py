afd5ae0da6e934a83383cc21482343828cadddb8
Smdonline <4cVVgJN5dZYUhtM>
2023-05-17 21:38:11 +0200

    Added user feed api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,7 +5,9 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('hello-viewset',views.HelloViewSet,basename='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
+router.register('feed',views.UserProfileFeedViewSet)
 urlpatterns = [
     path('hello-view/',views.HelloApiView.as_view(),name="hello-view"),
+    path('login/', views.UserLoginApiView.as_view()),
     path('',include(router.urls)),
 ]
