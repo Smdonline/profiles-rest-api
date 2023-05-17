d71fa9eaf2ba56f2e048cea7d344e09c3d01158b
Smdonline <4cVVgJN5dZYUhtM>
2023-05-17 19:14:13 +0200

    Added search filter view

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,6 +1,28 @@
 from rest_framework import serializers
+from profiles_api import models
 
 class HelloSerializer(serializers.Serializer):
     """Serialize a name input for testing out APIView   """
     name =serializers.CharField(max_length=10)
-    
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serialize a User Profile object"""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ['id','email','name','password']
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
+        return user
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 from profiles_api import serializers
 from rest_framework import viewsets
-
+from profiles_api import models
+from profiles_api import permissions
 class HelloApiView(APIView):
     """Test APIView"""
     serializer_class = serializers.HelloSerializer
@@ -72,7 +75,7 @@ class HelloViewSet(viewsets.ViewSet):
             )
     def retrive(self, request, pk=None):
         """hRetrive a object by it's ID"""
-        return Response({'method':'GET'})
+        return Response({'http_method':'GET'})
     def update(self, request, pk=None):
         """Handle updating an object"""
 
@@ -87,3 +90,13 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object"""
 
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Viewset for creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes  = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email')
